667e9aa093082f456f5c70f756f9d47deeb3fb6a
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-26 13:07:08 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   end 
   
   def match(array)
-    array.select do |the_word|
+    array.each do |the_word|
       split_word = the_word.split.sort
       original_word = @word.split.sort
       split_word == original_word ? the_word : []
