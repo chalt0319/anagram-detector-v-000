1610100f884ff67bb2bc33715b3e0b815e767ae7
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-26 13:16:45 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
     array.each do |the_word|
       split_word = the_word.split("").sort
       original_word = @word.split("").sort
-      binding.pry
+      # binding.pry
       # split_word == original_word ? the_word : []
       if split_word == original_word
         the_word
