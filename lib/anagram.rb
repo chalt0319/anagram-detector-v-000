15eddaa08dc25616f6f81e2b78d28952e4a98daf
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-26 13:20:28 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,7 +13,9 @@ class Anagram
       split_word = the_word.split("").sort
       original_word = @word.split("").sort
       # binding.pry
-      split_word == original_word ? the_word
+      if split_word == original_word
+        the_word
+      end 
     end 
     
   end 
