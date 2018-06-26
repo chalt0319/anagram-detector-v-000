e96ebc0c02df2f2b1b7bf2e0a559266b6a68e2b0
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-26 13:15:20 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,6 +12,7 @@ class Anagram
     array.each do |the_word|
       split_word = the_word.split("").sort
       original_word = @word.split("").sort
+      binding.pry
       split_word == original_word ? the_word : []
     end 
   end 
